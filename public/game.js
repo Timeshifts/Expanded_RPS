34f6688e7816dbce85058b18c18717fdae509530
Timeshifts <minjea441@gmail.com>
2023-12-05 17:46:03 +0900

    design page

--- public/game.js
+++ public/game.js
@@ -9,28 +9,36 @@ Object.freeze(GAME_STAGE)
 
 let select_hand = undefined;
 let other_hand = undefined;
+let state = undefined;
+
+function escape_game() {
+    if (state !== undefined) return;
+    $("#game_view").html(`<p>연결이 지연되고 있습니다...</p>
+    <br><button type="button" onclick="document.location = '/'">돌아가기</button><br>`);
+}
 
 function draw_game(state) {
     if (state === GAME_STAGE.WAIT_ANOTHER) {
         $("#game_view").html(`<p>다른 플레이어를 기다리는 중...</p>`);
+        setTimeout(escape_game, 1000);
     } else if (state === GAME_STAGE.CHOICE_HAND) {
         $("#game_view").html(`<h2>원하는 손을 선택하세요!</h2>
         <div id="hands"></div>`);
         $("#hands").html(`
-        <button type="button" id="button-scissors">
-            <img src="image/scissors.png" alt="가위" height="128" width="128" />
+        <button type="button" class="hands-button" id="button-scissors">
+            <img src="image/scissors.png" alt="가위" height="128" width="128" /><br>가위
         </button>
-        <button type="button" id="button-rock">
-            <img src="image/rock.png" alt="바위" height="128" width="128" />
+        <button type="button" class="hands-button" id="button-rock">
+            <img src="image/rock.png" alt="바위" height="128" width="128" /><br>바위
         </button>
-        <button type="button" id="button-paper">
-            <img src="image/paper.png" alt="보" height="128" width="128" />
+        <button type="button" class="hands-button" id="button-paper">
+            <img src="image/paper.png" alt="보" height="128" width="128" /><br>보
         </button>
-        <button type="button" id="button-lizard">
-            <img src="image/lizard.png" alt="도마뱀" height="128" width="128" />
+        <button type="button" class="hands-button" id="button-lizard">
+            <img src="image/lizard.png" alt="도마뱀" height="128" width="128" /><br>도마뱀
         </button>
-        <button type="button" id="button-spock">
-            <img src="image/spock.png" alt="스팍" height="128" width="128" />
+        <button type="button" class="hands-button" id="button-spock">
+            <img src="image/spock.png" alt="스팍" height="128" width="128" /><br>스팍
         </button>`);
     } else if (state === GAME_STAGE.WAIT_HAND) {
         $("#game_view").html(`<h2>상대방의 결정을 기다리는 중...</h2>
@@ -40,8 +48,11 @@ function draw_game(state) {
     } else if (state === GAME_STAGE.SHOW_RESULT) {
         $("#game_view").html(`<h2 id=result>결과</h2>
         <div>
+        <div id="result-detail">
         <img id="my-hand" height="128" width="128"/> vs
         <img id="other-hand" height="128" width="128"/>
+        </div>
+        <br>
         <button type="button" id="button-rematch">재대결</button>
         <button type="button" id="button-leave">나가기</button>
         </div>`)
@@ -50,7 +61,7 @@ function draw_game(state) {
 
 function choose_hand(socket, hand) {
     socket.emit('choose_hand', hand);
-    $("#game_view").html(`<h4>선택한 손: ${hand}</h4><h4>다른 플레이어를 기다리는 중...</h4>`)
+    $("#game_view").html(`<h4>선택한 손: ${hands[hand]}</h4><h4>다른 플레이어를 기다리는 중...</h4>`)
 }
 
 function rematch_request(socket) {
@@ -71,10 +82,18 @@ function change_state(socket, data) {
     } else if (state === GAME_STAGE.SHOW_RESULT) {
         $('#my-hand').attr('src', `image/${data.hands[0]}.png`);
         $('#other-hand').attr('src', `image/${data.hands[1]}.png`);
-        if (data.winner === 0) result_text = '무승부';
-        else if (data.winner === -1) result_text = '승리!';
-        else if (data.winner === 1) result_text = '패배';
+        if (data.winner === 0) {
+            result_text = '무승부';
+            result_color = 'black';
+        } else if (data.winner === -1) {
+            result_text = '승리!';
+            result_color = 'green'
+        } else if (data.winner === 1) {
+            result_text = '패배';
+            result_color = 'red'
+        }
         $('#result').html(result_text);
+        $('#result').css("color", result_color)
         $('#button-rematch').click(() => rematch_request(socket));
         $('#button-leave').click(() => leave(socket));
     }
